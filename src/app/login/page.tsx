c2d83b5692ead6dc20ecb9e54d0d3f9ec5e4d5f1
Sheldon Harding <sheldonharding@gmail.com>
2025-11-13 06:59:29 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: useStat

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
