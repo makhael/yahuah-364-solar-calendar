d63ecb1a8ea8e84078976bf70e4b62366a239d0d
Sheldon Harding <sheldonharding@gmail.com>
2025-11-13 04:47:37 +0000

    i am not seeing the add scripture section on the calendar page can you p

--- src/components/calendar/YahuahCalendar.tsx
+++ src/components/calendar/YahuahCalendar.tsx
@@ -2,6 +2,7 @@
 
 
 
+
 'use client';
 
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
@@ -136,6 +137,14 @@ export default function YahuahCalendar() {
 
                     {visibleSections.intro && (
                        <IntroSection openGlossaryModal={(termKey) => openModal('glossary', { termKey })} />
+                    )}
+
+                    {visibleSections.scripture && (
+                        <Card className="border shadow-lg" id="scripture-section">
+                             <CardContent className="p-4 sm:p-6">
+                                <ScriptureSubmission dateId={today ? today.toISOString().split('T')[0] : ''} />
+                            </CardContent>
+                        </Card>
                     )}
 
                     {visibleSections.podcast && (
